475a95deed01599f1cae2f27074bb365f192a027
Florie Anstett <florie.anstett@gmail.com>
2020-11-11 22:25:52 +0100

    suite css header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,28 @@
 import React from "react";
 import Logo from "../assets/Vinted_logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
     <header>
       <div className="container">
         <img src={Logo} alt="logo" />
-        <input
-          type="text"
-          id="search"
-          name="search"
-          placeholder="recherche des articles"
-        ></input>
-        <button>s'inscrire | se connecter</button>
-        <button>Vends tes articles</button>
+        <div className="intput">
+          <i className="inputLogo">
+            <FontAwesomeIcon icon="search" />
+          </i>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            placeholder="recherche des articles"
+          ></input>
+        </div>
+        <section>
+          <button>S'inscrire </button>
+          <button>Se connecter</button>
+          <button>Vends tes articles</button>
+        </section>
       </div>
     </header>
   );
