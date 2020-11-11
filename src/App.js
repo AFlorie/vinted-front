e9c7a6499f4c3be2224a18887fba9808fcd5ba15
Florie Anstett <florie.anstett@gmail.com>
2020-11-11 14:53:41 +0100

    ajout requete axios pour récupérer les offres

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import "./App.css";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./containers/Home";
 import Offer from "./containers/Offer";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [isLodading, setIsLoading] = useState(true);
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/offer/:id">
+          <Route path="/offer">
             <Offer />
           </Route>
           <Route path="/">
-            <Home />
+            <Home
+              articles={articles}
+              setArticles={setArticles}
+              isLodading={isLodading}
+              setIsLoading={setIsLoading}
+            />
           </Route>
         </Switch>
       </Router>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Axios from "axios";
 import { Link } from "react-router-dom";
 
 import Header from "../components/Header";
 import Heading from "../components/Heading";
 
-const Home = () => {
+const Home = ({ articles, setArticles, isLodading, setIsLoading }) => {
+  console.log(articles);
+  const fetchData = async () => {
+    const response = await Axios.get(
+      "https://lereacteur-vinted-api.herokuapp.com/offers"
+    );
+    setArticles(response.data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <Header />
