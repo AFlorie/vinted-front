202616edde4142e82ce076ca01565c384bb68726
Florie Anstett <florie.anstett@gmail.com>
2020-11-12 14:15:06 +0100

    modif css home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -32,28 +32,33 @@ const Home = () => {
       {isLoading ? (
         <p>Chargement en cours...</p>
       ) : (
-        <section className="articles-container container">
-          {articles.slice(0, 5).map((article, index) => {
-            return (
-              <Link key={index} to={`/offer/${article._id}`}>
-                <article className="article">
-                  <div className="userInfo">
-                    <img
-                      src={article.owner.account.avatar.secure_url}
-                      alt="profilPicture"
-                    />
-                    <span>{article.owner.account.username}</span>
-                  </div>
-                  <div className="articlePhoto">
-                    <img
-                      src={article.product_image.secure_url}
-                      alt="article à vendre"
-                    />
-                  </div>
-                </article>
-              </Link>
-            );
-          })}
+        <section className=" container">
+          <div className="articles-container">
+            {articles.map((article, index) => {
+              return (
+                <Link key={index} to={`/offer/${article._id}`}>
+                  <article className="article">
+                    <div className="userInfo">
+                      <img
+                        src={article.owner.account.avatar.secure_url}
+                        alt="profilPicture"
+                      />
+                      <span>{article.owner.account.username}</span>
+                    </div>
+                    <div className="articlePhoto">
+                      <img
+                        src={article.product_image.secure_url}
+                        alt="article à vendre"
+                      />
+                    </div>
+                    <p>{article.product_price} €</p>
+                    <p>{article.product_details[1].TAILLE}</p>
+                    <p>{article.product_details[0].MARQUE}</p>
+                  </article>
+                </Link>
+              );
+            })}
+          </div>
         </section>
       )}
     </>
