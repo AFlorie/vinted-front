4dba02321501249bac9b4ac3373e994cd9cf717d
Florie Anstett <florie.anstett@gmail.com>
2020-11-12 17:33:21 +0100

    ajout requete post signup

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,24 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await Axios.post(
+        "https://api-exo-vinted.herokuapp.com/user/signup",
+        {
+          usename: username,
+          email: email,
+          password: password,
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <section className="signup">
       <h2>S'inscrire</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           id="username"
           name="username"
           placeholder="Nom d'utilisateur"
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
         />
         <hr />
-        <input type="email" id="email" name="email" placeholder="Email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
         <hr />
         <input
           type="password"
           id="password"
           name="password"
           placeholder="Mot de passe"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
         <div className="largeCheckbox">
           <input
@@ -27,14 +66,14 @@ const Signup = () => {
             name="newsletter"
             className="checkbox"
           />
-          <label for="newsletter">S'inscrire à notre newsletter</label>
+          <label htmlFor="newsletter">S'inscrire à notre newsletter</label>
         </div>
         <p className="terms">
           En m'inscrivant je confirme avoir lu et accepté les Termes &
           Conditions et Politique de Confidentialité de Vinted. Je confirme
           avoir au moins 18 ans.
         </p>
-        <button>S'inscrire</button>
+        <button type="submit">S'inscrire</button>
         <p className="connect">Tu as déja un compte ? Connecte-toi !</p>
       </form>
     </section>
