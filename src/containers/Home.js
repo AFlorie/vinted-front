0c970bc931c63411cefde27b6a9431c2fe6e6a35
Florie Anstett <florie.anstett@gmail.com>
2020-11-13 16:29:04 +0100

    created new component TotalOffer to reduce Home.js code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import { Link } from "react-router-dom";
 
 import Heading from "../components/Heading";
+import TotalOffers from "../containers/TotalOffers";
 
 const Home = () => {
   const [articles, setArticles] = useState([]);
@@ -12,16 +12,15 @@ const Home = () => {
   const limit = 10;
   const [nbPages, setNbPages] = useState([]);
 
-  const getTotalPages = () => {
-    const totalPages = Math.ceil(totalOffers / limit);
-    const newTab = [...nbPages];
-    for (let i = 1; i < totalPages; i++) {
-      newTab.push(i);
-      setNbPages(newTab);
-    }
-  };
-
   useEffect(() => {
+    const getTotalPages = () => {
+      const totalPages = Math.ceil(totalOffers / limit);
+      const newTab = [...nbPages];
+      for (let i = 1; i < totalPages; i++) {
+        newTab.push(i);
+        setNbPages(newTab);
+      }
+    };
     getTotalPages(totalOffers, limit, nbPages);
   }, [totalOffers]);
 
@@ -49,32 +48,7 @@ const Home = () => {
         <p>Chargement en cours...</p>
       ) : (
         <section className=" container">
-          <div className="articles-container">
-            {articles.map((article, index) => {
-              return (
-                <Link key={index} to={`/offer/${article._id}`}>
-                  <article className="article">
-                    <div className="userInfo">
-                      <img
-                        src={article.owner.account.avatar.secure_url}
-                        alt="profilPicture"
-                      />
-                      <span>{article.owner.account.username}</span>
-                    </div>
-                    <div className="articlePhoto">
-                      <img
-                        src={article.product_image.secure_url}
-                        alt="article à vendre"
-                      />
-                    </div>
-                    <p className="price">{article.product_price} €</p>
-                    <p>{article.product_details[1].TAILLE}</p>
-                    <p>{article.product_details[0].MARQUE}</p>
-                  </article>
-                </Link>
-              );
-            })}
-          </div>
+          <TotalOffers articles={articles} />
           <div>
             {totalOffers &&
               nbPages.map((button, index) => {
---
+++ src/containers/TotalOffers.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const TotalOffers = ({ articles }) => {
+  return (
+    <div className="articles-container">
+      {articles.map((article, index) => {
+        return (
+          <Link key={index} to={`/offer/${article._id}`}>
+            <article className="article">
+              <div className="userInfo">
+                <img
+                  src={article.owner.account.avatar.secure_url}
+                  alt="profilPicture"
+                />
+                <span>{article.owner.account.username}</span>
+              </div>
+              <div className="articlePhoto">
+                <img
+                  src={article.product_image.secure_url}
+                  alt="article à vendre"
+                />
+              </div>
+              <p className="price">{article.product_price} €</p>
+              <p>{article.product_details[1].TAILLE}</p>
+              <p>{article.product_details[0].MARQUE}</p>
+            </article>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TotalOffers;
